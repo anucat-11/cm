5d3e38225adf41bd2e5cbee207bfdab3b934d4cc
anucat-11 <76250663+anucat-11@users.noreply.github.com>
2021-02-20 10:36:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var cat,mouse,bg;
+var catImg,mouseImg,bgImg;
+var catImg1,catImg2;
+
+function preload() {
+    //load the images here
+    catImg=loadImage("images/cat1.png");
+    mouseImg=loadImage("images/mouse3.png");
+    bgImg=loadImage("images/garden.png");
+    catImg1=loadImage("images/cat2.png");
+    catImg2=loadImage("images/cat4.png");
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+
+    //bg=createSprite(200,50,30,30);
+    //bg.addImage("bg",bgImg);
+    
+
+    cat=createSprite(390,550,20,20);
+    cat.addImage("cat",catImg);
+    cat.scale=0.1;
+
+    mouse=createSprite(200,550,30,30);
+    mouse.addImage("mouse",mouseImg);
+    mouse.scale=0.1;
+
+    
+}
+
+function draw() {
+
+    background(bgImg);
+    //Write condition here to evalute if tom and jerry collide
+
+    // text(mouseX + ',' + mouseY,10,45 );
+
+    if(cat.x-mouse.x < (cat.width-mouse.width)/2){
+
+        cat.addImage("lastImg",catImg2);
+        cat.changeImage("lastImg");
+        cat.velocityX=0;
+
+
+    }
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+  if(keyCode===LEFT_ARROW){
+      cat.velocityX=-5;
+     cat.addImage("running",catImg1);
+     cat.changeImage("running");
+  }
+}
